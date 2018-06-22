8ec47eed95f78482199d222e7bb1c546b1f34f65
christofr <pop.chris@gmail.com>
2018-06-22 20:43:05 +0100

    Initial

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h4 class="font-weight-bold py-3 mb-4">Home</h4>
+    <p>Elasticlab</p>
+    <p><button class="btn btn-primary btn-lg">Button</button></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  metaInfo: {
+    title: 'Home'
+  }
+}
+</script>
---
+++ src/components/Page2.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h4 class="font-weight-bold py-3 mb-4">Query</h4>
+    <p>Elasticlab</p>
+    <p><button class="btn btn-primary btn-lg">Button</button></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'page-2',
+  metaInfo: {
+    title: 'Page 2'
+  }
+}
+</script>
